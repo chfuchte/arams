377ac2454af4be1b81c89d2000e41bafa45a132f
Christian Fuchte <109520303+chfuchte@users.noreply.github.com>
2026-01-04 22:29:08 +0100

    update pnpm package

--- web/src/components/wasm-test.tsx
+++ web/src/components/wasm-test.tsx
@@ -1,6 +1,29 @@
-import { hello_from_wasm } from "@chfuchte/arams";
+import { check, run } from "@chfuchte/arams";
 import { Component } from "solid-js";
 
 export const WASMTest: Component = () => {
-    return <h1>{hello_from_wasm()}</h1>;
+    const code = `load 1
+            jzero return_one
+            sub #1
+            jzero return_one
+            load 1
+            store 2
+loop:       load 1
+            sub #1
+            jzero break
+            store 1
+            mul 2
+            store 2
+            goto loop
+return_one: load #1
+            end
+break:      load 2
+            end`;
+
+    const regs = new Map<Number, Number>();
+    regs.set(1, 5);
+
+    console.log(run(code, regs));
+
+    return <h1>{check("load 1") ? "Success" : "Failure"}</h1>;
 };
